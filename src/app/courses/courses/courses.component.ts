d93d1a0f66dd730b74a26755ab194d6d9099b7a7
ricajust <ricartoonstudio@gmail.com>
2025-04-19 20:24:02 -0300

    feat: separando os componentes do material em um model

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Course } from '../model/course';
 
 @Component({
   selector: 'app-courses',
@@ -7,7 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CoursesComponent implements OnInit {
 
-  constructor() { }
+  courses: Course[] = [
+    {_id: "1", name: "Angular", category: "Front-end"},
+    {_id: "2", name: "ReactJS", category: "Front-end"},
+    {_id: "3", name: "VueJS", category: "Front-end"},
+    {_id: "4", name: "Java", category: "Backend-end"},
+    {_id: "5", name: "SpringBoot", category: "Backend-end"},
+  ];
+  displayedColumns: string[] = ['name', 'category'];
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
