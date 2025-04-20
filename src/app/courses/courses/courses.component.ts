0f0de5b90be4a8414df047e6f22da6167cb1d9eb
ricajust <ricartoonstudio@gmail.com>
2025-04-19 22:41:54 -0300

    feat: ajuste da chamada de servico e tratamento de erro

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
+import { catchError, of } from 'rxjs';
 
 import { Course } from '../model/course';
 import { CoursesService } from '../services/courses.service';
+import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
 
 @Component({
   selector: 'app-courses',
@@ -15,9 +17,24 @@ export class CoursesComponent implements OnInit {
   courses: Course[] = []; // Tratamento genérico de dados para qq table
   displayedColumns: string[] = ['name', 'category'];
 
-  constructor(private coursesService: CoursesService) {
+  constructor(
+    private coursesService: CoursesService,
+    public dialog: MatDialog
+  ) {
     // this.courses$ = this.coursesService.list(); // Tratamento específico de dados para material table
-    this.coursesService.list().subscribe(courses => this.courses = courses); // Tratamento genérico de dados para qq table
+    this.coursesService
+      .list()
+      .pipe(catchError(error => {
+        this.onError("Erro ao carregar cursos.");
+        return of([]);
+      }))
+      .subscribe(courses => this.courses = courses); // Tratamento genérico de dados para qq table
+  }
+
+  onError(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg,
+    });
   }
 
   ngOnInit(): void {
